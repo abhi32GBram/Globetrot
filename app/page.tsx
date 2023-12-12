0a8eea188fc7b15893463b287b63d10fc60d3469
h6myman <abhiram.suradkar@gmail.com>
2023-12-12 18:06:35 +0530

    Added : Clerk (Authentication) Integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <p className='text-red-600 font-semibold'>Welcome to Globetrot</p>
+    <div className="flex flex-col gap-y-4">
+      <h1>
+        Dashboard 
+      </h1>
+      <UserButton  afterSignOutUrl="/"/>
+    </div>
   )
 }
