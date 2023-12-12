3a06810f769711ba621428eb4fba8916e126edd5
h6myman <abhiram.suradkar@gmail.com>
2023-12-12 22:03:18 +0530

    Added Dark Mode and Username Requirement (Clerk)

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["200", "300", "400", "500", "600", "700", "800"]
+})
+
+export const Logo = () => {
+    return (
+        <div className="flex flex-col items-center gap-y-4">
+            <div className="bg-white rounded-full p-1">
+                <Image src="/smile.svg" alt="Globetrot" height={80} width={80}  priority={true}/>
+            </div>
+            <div className="flex flex-col items-center ">
+                <p className={cn(
+                    "font-xl font-semibold",
+                    font.className
+                )}>
+                    Globetrot
+                </p>
+                <p className={cn(
+                    "text-sm text-muted-foreground",
+                    font.className
+                )}>
+                    The world is waiting, tune in...
+                </p>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
       <h1>
-        Dashboard 
+        Dashboard
       </h1>
-      <UserButton  afterSignOutUrl="/"/>
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
