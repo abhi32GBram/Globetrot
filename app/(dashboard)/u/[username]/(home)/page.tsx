931ea411dd548aaa1a90092cb7f3e5831c9ec08b
h6myman <abhiram.suradkar@gmail.com>
2023-12-19 19:23:36 +0530

    Fixing Page Types

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,26 +1,33 @@
-import { StreamPlayer } from '@/components/stream-player'
-import { getUserByUsername } from '@/lib/user-service'
-import { currentUser } from '@clerk/nextjs'
-import React from 'react'
+import { currentUser } from "@clerk/nextjs";
+
+import { getUserByUsername } from "@/lib/user-service";
+import { StreamPlayer } from "@/components/stream-player";
 
 interface CreatorPageProps {
     params: {
-        username: string
-    }
-}
-export const CreatorPage = async ({ params }: CreatorPageProps) => {
+        username: string;
+    };
+};
 
-    const externalUser = await currentUser()
-    const user = await getUserByUsername(params.username)
+const CreatorPage = async ({
+params,
+}: CreatorPageProps) => {
+    const externalUser = await currentUser();
+    const user = await getUserByUsername(params.username);
 
     if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
-        throw new Error("Unauthorized Access")
+        throw new Error("Unauthorized");
     }
+
     return (
-        <div className='h-full'>
-            <StreamPlayer user={user} stream={user.stream} isFollowing={true} />
+        <div className="h-full">
+            <StreamPlayer
+                user={user}
+                stream={user.stream}
+                isFollowing
+            />
         </div>
-    )
+    );
 }
 
-export default CreatorPage
+export default CreatorPage;
